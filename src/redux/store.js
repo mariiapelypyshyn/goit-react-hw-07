24ab74a8176cbb9e1d56486647c95957516e4d3e
mariiapelypyshyn <maria.peleshko97@gmail.com>
2024-11-06 20:39:37 +0200

    edited

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,16 @@
-import { configureStore } from "@reduxjs/toolkit";
+// import { configureStore } from "@reduxjs/toolkit";
 
 import { contactsReducer } from "./contactsSlice.";
 import { filterReducer } from "./filtersSlice";
 
+
+export const store = configureStore({
+    reducer: {
+        contacts: contactsReducer,
+        filter: filterReducer
+    }
+    
+})
 
 export const store = configureStore({
     reducer: {
